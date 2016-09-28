bc14a0749d596100db052710a70a01c3c3ca3131
Koichiro Okamoto <uxisfirst@gmail.com>
2016-09-28 18:53:14 +0900

    test: add component element testing

--- test/component/index/index.spec.ts
+++ test/component/index/index.spec.ts
@@ -7,6 +7,7 @@ import { BaseScope } from '../../../src/common/base'
 describe('index test', () => {
     let $injector: angular.auto.IInjectorService;
     let $controller: angular.IControllerService;
+    let $compile: angular.ICompileService;
     let controller: TestController;
     let scope: BaseScope;
     let rootScope: angular.IRootScopeService;
@@ -25,6 +26,7 @@ describe('index test', () => {
     beforeEach(() => {
         $injector = angular.injector(['ngMock']);
         $controller = $injector.get<angular.IControllerService>("$controller");
+        $compile = $injector.get<angular.ICompileService>("$compile");
         rootScope = $injector.get<angular.IRootScopeService>("$rootScope");
         scope = <any>rootScope.$new();
     })
@@ -39,5 +41,13 @@ describe('index test', () => {
 
         expect(controller.name).toBe('ko');
         expect(controller.age).toBe(32);
+    });
+
+    it('component: test template', () => {
+        let element = angular.element('<test name="ko" age="32"></test>');
+        let linkFn = $compile(element);
+        element = linkFn(scope);
+        expect(element.find('div').length === 2);
+        expect(element.find('span').length === 1);
     });
 });
